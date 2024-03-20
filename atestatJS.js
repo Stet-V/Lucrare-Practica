6ca38b7d7103c79c034f6844e2ff6483aba8d90f
Stet-V <119285183+Stet-V@users.noreply.github.com>
2024-03-20 19:15:14 +0200

    Add files via upload

--- atestatJS.js
+++ atestatJS.js
@@ -1,6 +1,19 @@
 var sidehover = document.getElementById("sidehover");
 var sidebar = document.getElementById("sidebar");
 
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    if (scrollPosition >= 200) {
+        console.log('yes');
+    } else {
+        console.log('yes');
+    }
+});
+
+sidehover.onmouseover = function() {
+    console.log('test');
+};
+
 sidehover.onmouseover = sidebar.onmouseover = function() {
     sidebar.style.left = "0px";
 };
