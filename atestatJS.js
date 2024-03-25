760a0f98ad629f9fb2b9b953d4cdc8efbe4406bd
Stet-V <119285183+Stet-V@users.noreply.github.com>
2024-03-25 18:37:31 +0200

    Add files via upload

--- atestatJS.js
+++ atestatJS.js
@@ -1,31 +1,9 @@
-var sidehover = document.getElementById("sidehover");
-var sidebar = document.getElementById("sidebar");
+function jumpTo(title){
 
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    if (scrollPosition >= 200) {
-        console.log('yes');
-    } else {
-        console.log('yes');
-    }
-});
-
-sidehover.onmouseover = function() {
-    console.log('test');
-};
-
-sidehover.onmouseover = sidebar.onmouseover = function() {
-    sidebar.style.left = "0px";
-};
-
-sidehover.onmouseout = sidebar.onmouseout = function() {
-    sidebar.style.left = "-300px";
-};
-
-function showPage(pageId) {
-    var pages = document.querySelectorAll('.page');
-    for (var i = 0; i < pages.length; i++) {
-        pages[i].classList.add('hidden');
-    }
-    document.getElementById(pageId).classList.remove('hidden');
+    var position = title.getBoundingClientRect().top;
+    var offsetPosition = position + window.scrollY - 128;
+  
+    window.scrollTo({
+        top: offsetPosition
+    });
 }
