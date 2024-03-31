76d609440240a50f323ab1fefef8c64a76701d51
Stet-V <119285183+Stet-V@users.noreply.github.com>
2024-03-31 17:27:49 +0300

    Delete atestatJS.js

--- atestatJS.js
+++
@@ -1,9 +0,0 @@
-function jumpTo(title){
-
-    var position = title.getBoundingClientRect().top;
-    var offsetPosition = position + window.scrollY - 128;
-  
-    window.scrollTo({
-        top: offsetPosition
-    });
-}
